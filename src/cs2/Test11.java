7e2543daf93f9852c089af28aa9f4757d2f55571
Your Name <email@example.com>
2022-10-02 01:52:12 +0800

    Initial commit

---
+++ src/cs2/Test11.java
@@ -0,0 +1,19 @@
+package cs2;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.text.ParseException;
+
+
+public class Test11 {
+    public static void main(String[] args) throws ParseException {
+        
+        Character cal = Character.getInstance()
+        int year = cal.get(calendr.YEAR);
+        
+        
+        
+    }
+}
